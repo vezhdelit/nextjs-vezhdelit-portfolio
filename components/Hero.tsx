cb8139570564da67cc3dde5872385b00049150fa
Vezhdel Vasyl <vezhdel.vasya2001@gmail.com>
2023-07-12 21:08:56 +0300

    feat: added buttons to Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,30 @@
 import Image from "next/image";
 import React from "react";
+import { CustomButton } from "./ui";
 
 const Hero = () => {
   return (
     <section id="hero" className="max-w-5xl mx-auto ">
-      <div className="flex flex-col items-center  justify-between md:flex-row py-32  px-5 md:px-0">
-        <div className=" w-full max-w-xl ">
-          <h1 className="text-6xl font-extrabold">Front-End React</h1>
-          <h1 className="text-6xl font-extrabold">Developer👨‍💻❤️⚛️</h1>
+      <div className="flex flex-col-reverse items-center  justify-between  py-5 md:flex-row md:py-32  px-5 lg:px-0">
+        <div className="  max-w-lg">
+          <h1 className="text-6xl font-extrabold">
+            Front-End React Developer 👨‍💻❤️⚛️
+          </h1>
+
           <p className=" text-xl text-gray-600 mt-5 dark:text-gray-300">
             Hi, I’m Vasyl. A passionate front-end developer from Uzhhgorod,
             Ukraine.📍
           </p>
+          <div className="flex flex-row  justify-between mt-10">
+            <CustomButton
+              text="Download CV"
+              className=" text-purple-600  border-2 border-purple-600  rounded-2xl text-xl font-bold min-w-[220px] min-h-[60px] "
+            />
+            <CustomButton
+              text="Contact me"
+              className=" text-white  bg-purple-600  rounded-2xl text-xl font-bold min-w-[220px] min-h-[60px] "
+            />
+          </div>
         </div>
 
         <div>
@@ -20,7 +33,7 @@ const Hero = () => {
             alt="headshot"
             width={396}
             height={396}
-            className="rounded-full"
+            className="rounded-full object-contain"
           />
         </div>
       </div>
--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,5 @@
+import { MouseEventHandler } from "react";
+
 export interface NavItem {
   title: string;
   id: string;
@@ -6,4 +8,13 @@ export interface NavItem {
 export interface MobileNavProps {
   items: NavItem[];
   toggleShowMobileMenu: () => void;
+}
+
+export interface CustomButtonProps {
+  isDisabled?: boolean;
+  btnType?: "button" | "submit";
+  className?: string;
+  text: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
