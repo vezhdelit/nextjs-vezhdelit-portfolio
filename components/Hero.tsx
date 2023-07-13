1582c1efcba22500e599e9f4fa031b01754a32a1
Vezhdel Vasyl <vezhdel.vasya2001@gmail.com>
2023-07-13 14:45:57 +0300

    fix: Hero.tsx adaptation fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,29 +5,29 @@ import { CustomButton } from "./ui";
 const Hero = () => {
   return (
     <section id="hero" className="max-w-5xl mx-auto ">
-      <div className="flex flex-col-reverse items-center  justify-between  py-5 md:flex-row md:py-32  px-5 lg:px-0">
-        <div className="  max-w-lg">
-          <h1 className="text-6xl font-extrabold">
-            Front-End React Developer 👨‍💻❤️⚛️
+      <div className="flex flex-col-reverse items-center  justify-between  py-8 md:flex-row md:py-32  px-5 lg:px-0">
+        <div className=" max-w-full sm:max-w-lg text-center md:text-left  space-y-8 ">
+          <h1 className="text-5xl font-extrabold md:text-6xl">
+            Front-End React Developer⚛️
           </h1>
-
-          <p className=" text-xl text-gray-600 mt-5 dark:text-gray-300">
+          <p className="text-xl text-gray-600 dark:text-gray-300">
             Hi, I’m Vasyl. A passionate front-end developer from Uzhhgorod,
             Ukraine.📍
           </p>
-          <div className="flex flex-row  justify-between mt-10">
+
+          <div className="flex space-x-6 justify-center md:justify-normal">
             <CustomButton
               text="Download CV"
-              className=" text-purple-600  border-2 border-purple-600  rounded-2xl text-xl font-bold min-w-[200px] md:min-w-[220px] min-h-[60px] "
+              className=" text-purple-600  border-2 border-purple-600  rounded-2xl text-xl font-bold w-full  max-w-[220px] min-h-[60px]  "
             />
             <CustomButton
               text="Contact me"
-              className=" text-white  bg-purple-600  rounded-2xl text-xl font-bold min-w-[200px] md:min-w-[220px]  min-h-[60px] "
+              className=" text-white  bg-purple-600  rounded-2xl text-xl font-bold w-full max-w-[220px] min-h-[60px] "
             />
           </div>
         </div>
 
-        <div>
+        <div className="max-md:w-60 max-md:h-60  my-8">
           <Image
             src="/headshot.png"
             alt="headshot"
